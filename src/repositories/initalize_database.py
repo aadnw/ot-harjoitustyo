53c1d6d09df128f54581a5201bff8be027afbd9e
Aada Nwajei <aada.nwajei@gmail.com>
2025-03-24 12:50:04 +0200

    Luo tietokannan alustustoimenpiteitä varten moduuli

---
+++ src/repositories/initalize_database.py
@@ -0,0 +1,37 @@
+from database_connection import get_database_connection
+
+def remove_tables(connection):
+    cursor = connection.cursor()
+
+    cursor.execute('''
+        drop table if exists users;
+        drop table if exists dreams;
+    ''')
+
+    connection.commit()
+
+def create_tables(connection):
+    cursor = connection.cursor()
+
+    cursor.execute('''
+        create table users (
+                   id integer primary key,
+                   username text,
+                   password text
+        );
+        create table dreams (
+                   id integer primary key,
+                   content text
+        );
+    ''')
+
+    connection.commit()
+
+def initialize_database():
+    connection = get_database_connection()
+
+    remove_tables(connection)
+    create_tables(connection)
+
+if __name__ == "__main__":
+    initialize_database()
