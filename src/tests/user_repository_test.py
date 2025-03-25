0400216615a5fd9f5d22a3bbe8bc0fa52877fa1c
Aada Nwajei <aada.nwajei@gmail.com>
2025-03-25 12:49:28 +0200

    Luo oma kansio testejä varten, ja sinne 1. testitiedosto

---
+++ src/tests/user_repository_test.py
@@ -0,0 +1,17 @@
+import unittest
+from repositories.user_repository import user_repository
+from entities.user import User
+
+class TestUserRepository(unittest.TestCase):
+    def setUp(self):
+        self.user_testaaja = User("testaaja", "testi123")
+        self.user_kukka = User("kukkis", "kukk4")
+
+    
+    def test_get_all_users(self):
+        user_repository.delete_all_users()
+        user_repository.create_user(self.user_testaaja)
+        user_repository.create_user(self.user_kukka)
+        result = user_repository.get_all_users()
+        
+        self.assertEqual([result[0].username, result[1].username], ['testaaja', 'kukkis'])
