b4d60b1f78115df5bf7e586ffb5b21c81c2531d9
Aada Nwajei <aada.nwajei@gmail.com>
2025-04-02 18:35:17 +0300

    id pakollinen tieto

--- src/tests/user_repository_test.py
+++ src/tests/user_repository_test.py
@@ -4,11 +4,14 @@ from entities.user import User
 
 
 class TestUserRepository(unittest.TestCase):
+    """Class for testing the UserRepository-class"""
+
     def setUp(self):
-        self.user_testaaja = User("testaaja", "testi123")
-        self.user_kukka = User("kukkis", "kukk4")
+        self.user_testaaja = User("1", "testaaja", "testi123")
+        self.user_kukka = User("2", "kukkis", "kukk4")
 
     def test_get_all_users(self):
+        """Test if get_all_users-funcion works properly"""
         user_repository.delete_all_users()
         user_repository.create_user(self.user_testaaja)
         user_repository.create_user(self.user_kukka)
