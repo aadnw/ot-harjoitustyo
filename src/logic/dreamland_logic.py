c5aeb60875e9f1b88e1cb2650f3f6a81512b99ca
Aada Nwajei <aada.nwajei@gmail.com>
2025-04-02 18:46:58 +0300

    Muutoksia funkioiden toiminnallisuuksiin

--- src/logic/dreamland_logic.py
+++ src/logic/dreamland_logic.py
@@ -1,22 +1,34 @@
 from entities.user import User
+from entities.dream import Dream
+
 from repositories.user_repository import (
     user_repository as default_user_repository)
 
+from repositories.dream_repository import (
+    dream_repository as default_dream_repository)
+
 
 class InvalidCredentialsError(Exception):
-    pass
+    """Error message about invalid credentials"""
+    
 
 
 class UsernameTakenError(Exception):
-    pass
+    """Error message about username being already taken"""
+    
 
 
 class DreamlandLogic:
-    def __init__(self, user_repository=default_user_repository):
+    """Sovelluslogiikasta vastaava luokka"""
+
+    def __init__(self, user_repository=default_user_repository,
+                 dream_repository=default_dream_repository):
         self._user = None
         self._user_repository = user_repository
+        self._dream_repository = dream_repository
 
     def login(self, username, password):
+        """Handle login, print error message if login fails"""
         user = self._user_repository.get_user_by_username(username)
 
         if not user or user.password != password:
@@ -27,18 +39,22 @@ class DreamlandLogic:
         return user
 
     def get_user(self):
+        """Return the current user"""
         return self._user
 
     def logout(self):
+        """Handle user logging out"""
         self._user = None
 
     def create_new_user(self, username, password, login=True):
+        """Create new user that includes a unique username that is 3-20 characters long and
+        a password that needs to be at least 5 characters long; if not, print error message"""
         username_exists = self._user_repository.get_user_by_username(username)
 
         if username_exists:
             raise UsernameTakenError("Tämä käyttäjänimi on jo käytetty")
 
-        user = self._user_repository.create_user(User(username, password))
+        user = self._user_repository.create_user(User(id, username, password))
 
         if not 3 <= len(user.username) <= 20:
             raise InvalidCredentialsError(
@@ -51,6 +67,24 @@ class DreamlandLogic:
         if login:
             self._user = user
         return user
+
+    def new_dream(self, content):
+        """Creates new dream throgh the sovelluslogiikka"""
+        dream = Dream(content=content, user=self._user)
+        return self._dream_repository.create_new_dream(dream)
+
+    def get_unachieved_dreams(self):
+        """Returns all the active dreams to show on homepage"""
+        if not self._user:
+            return []
+
+        dreams = self._dream_repository.get_all_dreams()
+
+        return list(filter(lambda dream: not dream.done, dreams))
+
+    def dream_achieved(self, dream_id):
+        """Dream achieved disappears from the homepage"""
+        self._dream_repository.set_dream_achieved(dream_id)
 
 
 dreamland_logic = DreamlandLogic()
