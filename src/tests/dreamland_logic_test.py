a72234fa1600c867f8effaa9ae84a58d72a75615
Aada Nwajei <aada.nwajei@gmail.com>
2025-04-09 15:23:29 +0300

    Lisää dreamland logic testejä

--- src/tests/dreamland_logic_test.py
+++ src/tests/dreamland_logic_test.py
@@ -96,3 +96,108 @@ class TestDreamlandLogic(unittest.TestCase):
                                             self.user_testaaja.password)
 
         self.assertEqual(result.username, self.user_testaaja.username)
+
+    def test_login_fails(self):
+        """Tests that logging in with invalid credentials prints an error message"""
+        self.assertRaises(InvalidCredentialsError,
+                          lambda: self.dreamland_logic.login("testaaja", "testi1"))
+
+    def test_get_user(self):
+        """Tests that get_user function returns the current user"""
+        self.dreamland_logic.create_new_user(self.user_testaaja.username,
+                                             self.user_testaaja.password)
+        self.dreamland_logic.login(self.user_testaaja.username,
+                                   self.user_testaaja.password)
+
+        result = self.dreamland_logic.get_user()
+
+        self.assertEqual(result.username, "testaaja")
+
+    def test_logout(self):
+        """Tests that logging out works as it should"""
+        self.dreamland_logic.create_new_user(self.user_testaaja.username,
+                                             self.user_testaaja.password)
+        self.dreamland_logic.login(self.user_testaaja.username,
+                                   self.user_testaaja.password)
+
+        self.assertEqual(self.dreamland_logic.logout(), None)
+
+    def test_create_new_user(self):
+        """Tests that creating a new user with valid credentials works as it should"""
+        self.dreamland_logic.create_new_user(self.user_testaaja.username,
+                                             self.user_testaaja.password)
+        self.dreamland_logic.login(self.user_testaaja.username,
+                                   self.user_testaaja.password)
+
+        result = self.dreamland_logic.get_user()
+        self.assertEqual(result.username, "testaaja")
+
+    def test_create_new_user_with_taken_username_fails(self):
+        """Tests that user can't register with a taken username"""
+        self.dreamland_logic.create_new_user(self.user_testaaja.username,
+                                             self.user_testaaja.password)
+
+        self.assertRaises(UsernameTakenError, lambda: self.dreamland_logic.create_new_user(
+            self.user_testaaja.username, "erisalasana"))
+
+    def test_create_new_user_with_too_short_username_fails(self):
+        """Tests that user can't register with a too short username"""
+        self.assertRaises(InvalidCredentialsError,
+                          lambda: self.dreamland_logic.create_new_user("te", "salasana"))
+
+    def test_create_new_user_with_too_long_username_fails(self):
+        """Tests that user can't register with a too long username"""
+        self.assertRaises(InvalidCredentialsError, lambda: self.dreamland_logic.create_new_user(
+            "Tämä on aivan liian pitkä", "salasana"))
+
+    def test_create_new_user_with_too_short_password_fails(self):
+        """Tests that user can't register with a too short password"""
+        self.assertRaises(InvalidCredentialsError,
+                          lambda: self.dreamland_logic.create_new_user("testaaja", "sala"))
+
+    def test_new_dream(self):
+        """Tests that adding a new dream works properly"""
+        self.dreamland_logic.create_new_user(self.user_testaaja.username,
+                                             self.user_testaaja.password)
+        self.dreamland_logic.login(self.user_testaaja.username,
+                                   self.user_testaaja.password)
+
+        self.dreamland_logic.new_dream("Testi")
+        dreams = self.dreamland_logic.get_unachieved_dreams()
+
+        self.assertEqual(len(dreams), 1)
+        self.assertEqual(dreams[0].content, "Testi")
+        self.assertEqual(dreams[0].user.username, self.user_testaaja.username)
+
+    def test_get_unachieved_dreams(self):
+        """Tests that the function returns the correct dreams (unachieved ones)"""
+        self.dreamland_logic.create_new_user(self.user_testaaja.username,
+                                             self.user_testaaja.password)
+        self.dreamland_logic.login(self.user_testaaja.username,
+                                   self.user_testaaja.password)
+
+        self.dreamland_logic.new_dream("Testi")
+        result = self.dreamland_logic.get_unachieved_dreams()
+
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].content, "Testi")
+
+    def test_if_user_not_logged_in_no_dreams_are_returned(self):
+        """Tests that get_unachieved_dreams -function doesn't work if user's not logged in"""
+        self.dreamland_logic.new_dream("Testi")
+        result = self.dreamland_logic.get_unachieved_dreams()
+
+        self.assertEqual(result, [])
+
+    def test_dream_achieved(self):
+        """Tests that marking a dream achieved works as it should"""
+        self.dreamland_logic.create_new_user(self.user_testaaja.username,
+                                             self.user_testaaja.password)
+        self.dreamland_logic.login(self.user_testaaja.username,
+                                   self.user_testaaja.password)
+
+        self.dreamland_logic.new_dream("Testi")
+        result = self.dreamland_logic.get_unachieved_dreams()
+        self.dreamland_logic.dream_achieved(result[0].id)
+
+        self.assertEqual(len(self.dreamland_logic.get_unachieved_dreams()), 0)
