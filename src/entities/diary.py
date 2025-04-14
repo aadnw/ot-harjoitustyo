9110686eb3676ddcf7b9df5ea87f1ce6cdc708f3
Aada Nwajei <aada.nwajei@gmail.com>
2025-04-14 16:20:16 +0300

    Luo diary.py tiedosto jossa uusi Diary-luokka

---
+++ src/entities/diary.py
@@ -0,0 +1,9 @@
+"""This module includes the Diary class that takes care of storing all diary notes"""
+
+
+class Diary:
+    """Class taking care of describing diary notes"""
+
+    def __init__(self, content, dream_id=None):
+        self.content = content
+        self.id = dream_id
