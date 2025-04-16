9fb2ee228211eb77dae7695ce0c214df974a479f
Aada Nwajei <aada.nwajei@gmail.com>
2025-04-16 15:25:36 +0300

    Luo data kansion jos sitä ei ole

--- src/build.py
+++ src/build.py
@@ -1,10 +1,14 @@
 """This module initializes the database"""
+import os
 
 from initalize_database import initialize_database
+from config import DATA_FOLDER
 
 
 def build():
     """Create the database"""
+    if not os.path.isdir(DATA_FOLDER):
+        os.mkdir(DATA_FOLDER)
     initialize_database()
 
 
