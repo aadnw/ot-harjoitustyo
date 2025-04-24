01c35dcbbcdd64c8d7a6f103d89943421b0b8929
Aada Nwajei <aada.nwajei@gmail.com>
2025-04-24 15:54:14 +0300

    Poista dream taulu

--- src/initalize_database.py
+++ src/initalize_database.py
@@ -8,7 +8,6 @@ def remove_tables(connection):
     cursor = connection.cursor()
 
     cursor.execute("drop table if exists users")
-    cursor.execute("drop table if exists dreams")
     cursor.execute("drop table if exists diary")
 
     connection.commit()
@@ -23,15 +22,6 @@ def create_tables(connection):
                    id integer primary key,
                    username text,
                    password text
-        )''')
-    cursor.execute('''
-        create table dreams (
-                   id integer primary key autoincrement,
-                   content text,
-                   user_id integer,
-                   done boolean default 0, --Default to 0 (False) if not provided,
-                   star integer,
-                   foreign key (user_id) references users (id) on delete cascade
         )''')
     cursor.execute('''
         create table diary (
