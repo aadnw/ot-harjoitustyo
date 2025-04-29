0da7407c59572d9b137d8588f168cc8b8dc72e73
Aada Nwajei <aada.nwajei@gmail.com>
2025-04-29 13:28:07 +0300

    Lisätty funktio joka luo popup windown

--- src/ui/forms.py
+++ src/ui/forms.py
@@ -21,3 +21,23 @@ class FormHandler:
     def show_frame(self):
         """Expose _frame via getter"""
         return self.frame
+
+    def set_buttons_for_confirmation(self, tk, popup, _yes, _no):
+        """Creates the frame and buttons for the confirmation popup windows
+        
+        Args:
+            tk: tkinter imported as tk
+            popup: variable for the popup window
+            _yes: function that is called when the yes button is clicked
+            _no: function that is called when the no button is clicked
+        """
+        button_frame = tk.Frame(popup, bg="#D0F1FF")
+        button_frame.pack(pady=10)
+
+        yes_button = tk.Button(button_frame, text="Kyllä", font=("Bookman"),
+                               bg="#FADCD9", fg="#00044A", width=10, command=_yes)
+        yes_button.grid(row=0, column=0, padx=5)
+
+        no_button = tk.Button(button_frame, text="Ei", font=("Bookman"),
+                              bg="#FADCD9", fg="#00044A", width=10, command=_no)
+        no_button.grid(row=0, column=1, padx=5)
