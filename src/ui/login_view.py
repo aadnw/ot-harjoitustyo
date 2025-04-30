564dcdcc9c0cbb45c538d07acba9c85a2878d082
Aada Nwajei <aada.nwajei@gmail.com>
2025-04-30 12:41:15 +0300

    Lisää kirjautumissivulle haavemaa logo kuva

--- src/ui/login_view.py
+++ src/ui/login_view.py
@@ -3,6 +3,8 @@ that can be done on the login page"""
 
 from tkinter import StringVar, Label
 import tkinter as tk
+import os
+from PIL import Image, ImageTk
 from ui.forms import FormHandler
 from logic.dreamland_logic import dreamland_logic, InvalidCredentialsError
 
@@ -63,24 +65,23 @@ class LoginView:
         """
 
         self.form_handler.error_variable.set(message)
-        self.form_handler.error_label.grid()
 
     def _initialize_username_field(self):
         """Initializes the field where the user can write their username"""
-        tk.Label(self.form_handler.frame, text="Käyttäjänimi", font=("Bookman", 12), fg="#00044A",
+        tk.Label(self.left_frame, text="Käyttäjänimi", font=("Bookman", 12), fg="#00044A",
                  bg="#D0F1FF").grid(row=1, column=0, columnspan=2, sticky="w", pady=(5, 0))
 
-        self.form_handler.username_entry = tk.Entry(self.form_handler.frame, width=25, font=(
+        self.form_handler.username_entry = tk.Entry(self.left_frame, width=25, font=(
             "Bookman", 12), bd=2, relief="solid", bg="#DFF7FF")
         self.form_handler.username_entry.grid(row=2, column=0, columnspan=2, pady=5)
 
     def _initialize_password_field(self):
         """Initializes the field where the user can write their password"""
-        tk.Label(master=self.form_handler.frame, text="Salasana", font=("Bookman", 12),
+        tk.Label(self.left_frame, text="Salasana", font=("Bookman", 12),
                  fg="#00044A", bg="#D0F1FF").grid(row=3, column=0,
                                                   columnspan=2, sticky="w", pady=(10, 0))
 
-        self.form_handler.password_entry = tk.Entry(master=self.form_handler.frame, width=25, font=(
+        self.form_handler.password_entry = tk.Entry(self.left_frame, width=25, font=(
             "Bookman", 12), bd=2, relief="solid", bg="#DFF7FF", show="*")
         self.form_handler.password_entry.grid(row=4, column=0, columnspan=2, pady=5)
 
@@ -88,26 +89,42 @@ class LoginView:
         """Initializes the login page's ui"""
         self.form_handler.frame = tk.Frame(self._root, bg="#D0F1FF", padx=20, pady=20)
         self.form_handler.frame.place(relx=0.7, rely=0.4, anchor="center")
-        Label(self.form_handler.frame, text="Kirjaudu sisään", font=("Bookman", 20, "bold"),
+
+        left_frame = tk.Frame(self.form_handler.frame, bg ="#D0F1FF")
+        left_frame.grid(row=0, column=0, sticky="n")
+        self.left_frame = left_frame
+
+        right_frame = tk.Frame(self.form_handler.frame, bg="#D0F1FF")
+        right_frame.grid(row=0, column=1, padx=(50, 0), sticky="n")
+
+        Label(left_frame, text="Kirjaudu sisään", font=("Bookman", 20, "bold"),
               fg="#00044A", bg="#D0F1FF").grid(row=0, column=0, columnspan=2, pady=10)
 
-        self.form_handler.error_variable = StringVar(self.form_handler.frame)
-
-        self.form_handler.error_label = tk.Label(master=self.form_handler.frame,
+        self.form_handler.error_variable = StringVar()
+        self.form_handler.error_label = tk.Label(right_frame,
                                      textvariable=self.form_handler.error_variable,
                                      font=("Bookman", 15, "bold"), bg="#D0F1FF", fg="#FC2D7D")
-        self.form_handler.error_label.grid(row=0, column=5, pady=5)
+        self.form_handler.error_label.grid(row=0, column=0, pady=10)
 
         self._initialize_username_field()
         self._initialize_password_field()
 
-        login_button = tk.Button(master=self.form_handler.frame, text="Kirjaudu sisään",
+        login_button = tk.Button(left_frame, text="Kirjaudu sisään",
                                  font=("Bookman", 12), bg="#FADCD9", fg="#00044A", padx=20,
                                  pady=5, borderwidth=0, command=self._login_handler)
         login_button.grid(row=6, column=0, columnspan=2, pady=10)
 
-        registration_button = tk.Button(master=self.form_handler.frame, text="Rekisteröidy tästä",
+        registration_button = tk.Button(left_frame, text="Rekisteröidy tästä",
                                         font=("Bookman", 14, "bold"),
                                         bg="#FADCD9", fg="#00044A", padx=30, pady=8, borderwidth=0,
                                         command=self._handle_registration_view)
         registration_button.grid(row=10, column=0, columnspan=2, pady=10)
+
+        image_path = os.path.join(os.path.dirname(__file__), "..", "images", "haavemaa_logo.png")
+        logo = ImageTk.PhotoImage(Image.open(image_path))
+        self.logo_image = logo
+        logo_label = Label(right_frame, image=self.logo_image, bg="#D0F1FF")
+        logo_label.grid(row=1, column=0, sticky="nsew", padx=60)
+
+        right_frame.columnconfigure(0, weight=1)
+        right_frame.rowconfigure(1, weight=1)
